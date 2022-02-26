114a709f2c1b9cdadab87b1d3050a99c85dd64c2
Akira Komamura <akira.komamura@gmail.com>
2022-02-26 17:16:18 +0900

    Add main nix26.el

--- nix26-flake.el
+++ nix26-flake.el
@@ -1,35 +1,5 @@
 ;;; nix26-flake.el --- Support for Nix flakes -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022 Akira Komamura
-
-;; Author: Akira Komamura <akira.komamura@gmail.com>
-;; Version: 0.1
-;; Package-Requires: ((emacs "26.1") (promise "1.1") (project "0.6") (magit-section "3.3"))
-;; Keywords: processes
-;; URL: https://github.com/emacs-twist/nix26.el
-
-;; This file is not part of GNU Emacs.
-
-;;; License:
-
-;;; Commentary:
-
-;; This library is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU Lesser General Public
-;; License as published by the Free Software Foundation; either
-;; version 2.1 of the License, or (at your option) any later version.
-;;
-;; This library is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; Lesser General Public License for more details.
-;;
-;; You should have received a copy of the GNU Lesser General Public
-;; License along with this library; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-;;; Code:
-
 (require 'nix26-repl)
 (require 'nix26-utils)
 (require 'nix26-flake-input)
---
+++ nix26.el
@@ -0,0 +1,34 @@
+;;; nix26.el --- Frontend for experimental commands of Nix -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "26.1") (promise "1.1") (project "0.6") (magit-section "3.3") (s "1.12"))
+;; Keywords: processes
+;; URL: https://github.com/emacs-twist/nix26.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;;; Commentary:
+
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 2.1 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+;;; Code:
+
+(provide 'nix26)
+;;; nix26.el ends here
