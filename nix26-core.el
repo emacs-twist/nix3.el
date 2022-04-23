10ccd6b038d0cf6e7d0b0247bfaa940b441044da
Akira Komamura <akira.komamura@gmail.com>
2022-04-24 01:14:26 +0900

    refactor: Factor out nix26-read-nix-command function

--- nix26-core.el
+++ nix26-core.el
@@ -9,15 +9,27 @@
   ""
   :type 'file)
 
+(defun nix26-read-nix-command (&rest args)
+  "Run nix and return its output string."
+  (with-temp-buffer
+    (let ((err-file (make-temp-file "nix26")))
+      (unwind-protect
+          (unless (zerop (apply #'call-process
+                                nix26-nix-executable
+                                nil (list (current-buffer) err-file) nil
+                                args))
+            (error "Failed to run %s: %s"
+                   (cons nix26-nix-executable args)
+                   (with-temp-buffer
+                     (insert-file-contents err-file)
+                     (buffer-string))))
+        (delete-file err-file)))
+    (buffer-string)))
+
 (defun nix26-system ()
   "Return the system name of Nix."
-  (with-temp-buffer
-    (unless (zerop (call-process nix26-nix-executable
-                                 nil (list (current-buffer) nil) nil
-                                 "eval"
-                                 "--expr" "builtins.currentSystem"
-                                 "--impure" "--raw"))
-      (error "Failed to run nix eval"))))
+  (nix26-read-nix-command "eval" "--expr" "builtins.currentSystem" "--impure"
+                          "--raw"))
 
 (defun nix26-normalize-path (dir)
   (string-remove-suffix "/" (file-truename dir)))
