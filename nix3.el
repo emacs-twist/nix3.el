20add9ee72cdabe5782ebc2c015c4d7a02e7e2b0
Akira Komamura <akira.komamura@gmail.com>
2022-11-30 08:35:07 +0900

    Fix the library header

--- nix3.el
+++ nix3.el
@@ -13,8 +13,6 @@
 
 ;;; License:
 
-;;; Commentary:
-
 ;; This library is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public
 ;; License as published by the Free Software Foundation; either
@@ -28,6 +26,8 @@
 ;; You should have received a copy of the GNU Lesser General Public
 ;; License along with this library; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+;;; Commentary:
 
 ;;; Code:
 
