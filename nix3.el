4ca8961a0a3d8ba27d3b2e8b445cea93eb902eb6
Akira Komamura <akira.komamura@gmail.com>
2023-01-17 06:50:06 +0900

    fix: Put all Package-Requires entries on a single line (#2)
    Even though package.el accepts Package-Requires entries that spans across
    multiple lines, it must be on a single line, according to `elisp#Library
    Headers'.

--- nix3.el
+++ nix3.el
@@ -4,8 +4,7 @@
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
 ;; Version: 0.1
-;; Package-Requires: ((emacs "26.1") (promise "1.1") (project "0.6")
-;;                    (magit-section "3.3") (s "1.12"))
+;; Package-Requires: ((emacs "26.1") (promise "1.1") (project "0.6") (magit-section "3.3") (s "1.12"))
 ;; Keywords: processes
 ;; URL: https://github.com/emacs-twist/nix3.el
 
