4bb71991b87ec88d6af09a4257fd84f196880fc4
Akira Komamura <akira.komamura@gmail.com>
2023-04-10 02:13:34 +0900

    Add a library header to each non-main file

--- nix3-registry.el
+++ nix3-registry.el
@@ -1,5 +1,34 @@
 ;;; nix3-registry.el --- A wrapper for Nix registries -*- lexical-binding: t -*-
 
+;; Copyright (C) 2022,2023 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; URL: https://github.com/emacs-twist/nix3.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 2.1 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+;;; Commentary:
+
+;; FIXME
+
+;;; Code:
+
 (require 'nix3-core)
 (require 'nix3-utils)
 (require 'subr-x)
