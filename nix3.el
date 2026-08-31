5692df72382275de733d7d3e30f9b80343a7ba9a
Akira Komamura <akira.komamura@gmail.com>
2026-08-31 12:13:12 +0900

    fix: Add Commentary

--- nix3.el
+++ nix3.el
@@ -29,6 +29,10 @@
 
 ;;; Commentary:
 
+;; This library provides interactive Emacs commands for working with Nix
+;; flakes.  It offers commands to inspect flake outputs, build derivations,
+;; and run flake applications through a transient interface.
+
 ;;; Code:
 
 (require 'nix3-core)
